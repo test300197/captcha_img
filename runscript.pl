0f4756e4fcca2f4eb2718e54bacd2650bddcc8e5
Nikolay Zvonov <test300197@gmail.com>
2018-07-17 11:41:05 +0300

    Refactor code

--- runscript.pl
+++ runscript.pl
@@ -2,7 +2,7 @@ use strict;
 use utf8;
 use open ':std', ':encoding(UTF-8)';
 
-my $name_img = 'clean-img.jpg';
+my $name_img = 'cat4-img.jpg';
 my $name_out = 'text';
 my $text_out;
 my $cmd = 'tesseract ' . $name_img . ' '. $name_out . ' -l rus';
@@ -11,8 +11,7 @@ system($cmd);
 
 my $filename = $name_out .'.txt';
 if (open(my $text, '<:encoding(UTF-8)', $filename)) {
-  my $row = <$text>;
-  $text_out = $row;
+  $text_out = <$text>;
   close($text);
 }
 
